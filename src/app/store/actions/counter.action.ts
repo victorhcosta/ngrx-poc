e286113aa342199f0e12354638cb77b090df247e
victorhcosta <victor.v.h.o.c@outlook.com>
2020-07-19 04:32:08 -0300

    removendo import e export não usado

--- src/app/store/actions/counter.action.ts
+++ src/app/store/actions/counter.action.ts
@@ -1,7 +1,5 @@
 import { createAction, props } from '@ngrx/store';
 
-import { IAction } from 'src/app/models/action';
-
 const increment = createAction('Increment', props<{ payload: number }>());
 
 const decrement = createAction('Decrement', props<{ payload: number }>());
@@ -9,9 +7,3 @@ const decrement = createAction('Decrement', props<{ payload: number }>());
 const reset = createAction('Reset');
 
 export const counterAction = { increment, decrement, reset };
-
-export enum CountAction {
-	Increment = 'Increment',
-	Decrement = 'Decrement',
-	Reset = 'Reset',
-}
